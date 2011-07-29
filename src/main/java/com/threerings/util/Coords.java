167cf5dc0a419d5b04943233cc04430e6e70ead0
Michael Bayne <mdb@samskivert.com>
2011-07-29 10:32:42 -0700

    Utility routines for converting from layer to screen coordinates, &c.

---
+++ src/main/java/com/threerings/util/Coords.java
@@ -0,0 +1,67 @@
+//
+// Triple Play - utilities for use in ForPlay-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package com.threerings.util;
+
+import forplay.core.Layer;
+
+import pythagoras.f.AffineTransform;
+import pythagoras.f.IPoint;
+import pythagoras.f.Point;
+
+/**
+ * Coordinate-related utility methods.
+ */
+public class Coords
+{
+    /**
+     * Converts the supplied point from coordinates relative to the specified layer to screen
+     * coordinates. The results are stored into {@code into}, which is returned for convenience.
+     */
+    public static Point layerToScreen (Layer layer, IPoint point, Point into) {
+        return layerToParent(layer, null, point, into);
+    }
+
+    /**
+     * Converts the supplied point from coordinates relative to the specified child layer to
+     * coordinates relative to the specified parent layer. The results are stored into {@code
+     * into}, which is returned for convenience.
+     */
+    public static Point layerToParent (Layer layer, Layer parent, IPoint point, Point into) {
+        into.set(point);
+        while (layer != parent) {
+            if (layer == null) {
+                throw new IllegalArgumentException(
+                    "Failed to find parent, perhaps you passed parent, layer instead of "+
+                    "layer, parent?");
+            }
+            into.x -= layer.originX();
+            into.y -= layer.originY();
+            forplay.core.Transform lt = layer.transform();
+            _scratch.setTransform(lt.m00(), lt.m10(), lt.m01(), lt.m11(), lt.tx(), lt.ty());
+            _scratch.transform(into, into);
+            layer = layer.parent();
+        }
+        return into;
+    }
+
+    /**
+     * Converts the supplied point from screen coordinates to coordinates relative to the specified
+     * layer. The results are stored into {@code into}, which is returned for convenience.
+     */
+    public static Point screenToLayer (Layer layer, IPoint point, Point into) {
+        Layer parent = layer.parent();
+        IPoint cur = (parent == null) ? point : screenToLayer(parent, point, into);
+        forplay.core.Transform lt = layer.transform();
+        _scratch.setTransform(lt.m00(), lt.m10(), lt.m01(), lt.m11(), lt.tx(), lt.ty());
+        into = _scratch.inverseTransform(cur, into);
+        into.x += layer.originX();
+        into.y += layer.originY();
+        return into;
+    }
+
+    /** A scratch transform, used by {@link #screenToLayer} and {@link #layerToScreen}. */
+    protected static AffineTransform _scratch = new AffineTransform();
+}
