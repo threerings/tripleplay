2847790b540737b68e2cddcb4c3e1d85034b3f5a
Tim Conkling <tconkling@gmail.com>
2011-08-16 14:03:16 -0700

    JsonUtil.getArrayObjects

--- src/main/java/tripleplay/util/JsonUtil.java
+++ src/main/java/tripleplay/util/JsonUtil.java
@@ -5,6 +5,7 @@
 
 package tripleplay.util;
 
+import java.util.Iterator;
 import playn.core.Json;
 
 /**
@@ -18,5 +19,45 @@ public class JsonUtil
     public static <T extends Enum<T>> T getEnum (Json.Object json, String key, Class<T> enumType)
     {
         return Enum.valueOf(enumType, json.getString(key));
+    }
+
+    /**
+     * @return an Iterable<Json.Object> that iterates the Objects in the Array at the given key,
+     * or null if there's no Array at that key.
+     *
+     * The Iterable will throw an error, during iteration, if any of the items in the Array are
+     * not Json.Objects.
+     */
+    public static Iterable<Json.Object> getArrayObjects (Json.Object json, String key)
+    {
+        final Json.Array array = json.getArray(key);
+        if (array == null) {
+            return null;
+
+        } else {
+            return new Iterable<Json.Object>() {
+                @Override public Iterator<Json.Object> iterator () {
+                    return new Iterator<Json.Object>() {
+                        @Override public boolean hasNext () {
+                            return _index < array.length();
+                        }
+                        @Override public Json.Object next () {
+                            Json.Object obj = array.getObject(_index++);
+                            if (obj == null) {
+                                throw new RuntimeException("There's no Json.Object at the given " +
+                                        "index [index=" + _index + "]");
+                            }
+                            ++_index;
+                            return obj;
+                        }
+                        @Override public void remove () {
+                            throw new UnsupportedOperationException();
+                        }
+
+                        protected int _index;
+                    };
+                }
+            };
+        }
     }
 }
