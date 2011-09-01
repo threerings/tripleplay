d5f1c7c1cb258cb4a985489cb08d7b6c5d284a23
Michael Bayne <mdb@samskivert.com>
2011-09-01 16:18:34 -0700

    Added Logger facade.
    This can be used to route to PlayN logging on the client, stderr in unit tests,
    and whatever is desired on the server. We specifically avoid classpath or other
    magic, because that is often the cause of more pain than pleasure with other
    too-clever-for-their-own-good logging frameworks.

---
+++ src/main/java/tripleplay/util/Logger.java
@@ -0,0 +1,151 @@
+//
+// Triple Play - utilities for use in PlaY-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+/**
+ * Provides logging services that are routed to the appropriate logging destination on the client
+ * or server. A useful usage pattern is for a library or game to define a shared {@code Log} class
+ * like so:
+ *
+ * <pre>{@code
+ * import tripleplay.util.Logger;
+ * public class Log {
+ *   public static final Logger log = new Logger("libident");
+ * }</pre>
+ *
+ * and then import {@code Log.log} statically into classes so that they can invoke {@code
+ * log.info}, etc.
+ */
+public class Logger
+{
+    /**
+     * Wires the logging front-end to the logging back-end. See {@link #setImpl}.
+     */
+    public interface Impl {
+        void debug (String message, Throwable t);
+        void info (String message, Throwable t);
+        void warning (String message, Throwable t);
+    }
+
+    /**
+     * Configures the logging back-end. This should be called before any code that makes use of the
+     * logging services. The default back-end logs to {@code stderr}, which is useful when running
+     * in unit tests.
+     */
+    public static void setImpl (Impl impl) {
+        _impl = impl;
+    }
+
+    /**
+     * Formats and returns the supplied key/value arguments as {@code [key=value, key=value, ...]}.
+     */
+    public static String format (Object... args) {
+        return format(new StringBuilder("["), args).append("]").toString();
+    }
+
+    /**
+     * Formats the supplied key/value arguments into the supplied string builder as {@code
+     * key=value, key=value, ...}.
+     * @return the supplied string builder.
+     */
+    public static StringBuilder format (StringBuilder into, Object... args) {
+        for (int ii = 0, ll = args.length/2; ii < ll; ii++) {
+            if (ii > 0) {
+                into.append(", ");
+            }
+            into.append(args[2*ii]).append("=").append(args[2*ii+1]);
+        }
+        return into;
+    }
+
+    /**
+     * Creates a logger with the specified ident string.
+     */
+    public Logger (String ident) {
+        _ident = ident;
+    }
+
+    /**
+     * Logs a debug message.
+     *
+     * @param message the text of the message.
+     * @param args a series of zero or more key/value pairs followed by an optional {@link
+     * Throwable} cause.
+     */
+    public void debug (String message, Object... args) {
+        log(DEBUG_TARGET, _ident, message, args);
+    }
+
+    /**
+     * Logs an info message.
+     *
+     * @param message the text of the message.
+     * @param args a series of zero or more key/value pairs followed by an optional {@link
+     * Throwable} cause.
+     */
+    public void info (String message, Object... args) {
+        log(INFO_TARGET, _ident, message, args);
+    }
+
+    /**
+     * Logs a warning message.
+     *
+     * @param message the text of the message.
+     * @param args a series of zero or more key/value pairs followed by an optional {@link
+     * Throwable} cause.
+     */
+    public void warning (String message, Object... args) {
+        log(WARNING_TARGET, _ident, message, args);
+    }
+
+    protected final String _ident;
+
+    protected static void log (Target target, String ident, String message, Object... args) {
+        StringBuilder sb = new StringBuilder().append(ident).append(": ").append(message);
+        if (args.length > 1) {
+            sb.append(" [");
+            format(sb, args);
+            sb.append("]");
+        }
+        Object error = (args.length % 2 == 1) ? args[args.length-1] : null;
+        target.log(sb.toString(), (Throwable)error);
+    }
+
+    protected static interface Target {
+        void log (String message, Throwable t);
+    }
+
+    protected static Target DEBUG_TARGET = new Target() {
+        public void log (String message, Throwable t) {
+            _impl.debug(message, t);
+        }
+    };
+    protected static Target INFO_TARGET = new Target() {
+        public void log (String message, Throwable t) {
+            _impl.info(message, t);
+        }
+    };
+    protected static Target WARNING_TARGET = new Target() {
+        public void log (String message, Throwable t) {
+            _impl.warning(message, t);
+        }
+    };
+
+    protected static Impl _impl = new Impl() {
+        public void debug (String message, Throwable t) {
+            info(message, t);
+        }
+        public void info (String message, Throwable t) {
+            System.out.println(message);
+            if (t != null) {
+                t.printStackTrace(System.out);
+            }
+        }
+        public void warning (String message, Throwable t) {
+            info(message, t);
+        }
+    };
+}
---
+++ src/test/java/tripleplay/util/LoggerTest.java
@@ -0,0 +1,66 @@
+//
+// Triple Play - utilities for use in PlaY-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Tests aspects of the {@link Logger} class.
+ */
+public class LoggerTest
+{
+    @Test
+    public void testOutput () {
+        StringWriter buf = configWriterImpl();
+        Logger log = new Logger("test");
+        log.info("This is a test.");
+        assertEquals("test: This is a test.\n", buf.toString());
+    }
+
+    @Test
+    public void testParams () {
+        StringWriter buf = configWriterImpl();
+        Logger log = new Logger("test");
+        log.info("Foo.", "bar", "baz", "one", 1, "null", null);
+        assertEquals("test: Foo. [bar=baz, one=1, null=null]" + NEWLINE, buf.toString());
+    }
+
+    @Test
+    public void testException () {
+        StringWriter buf = configWriterImpl();
+        Logger log = new Logger("test");
+        log.info("Foo.", "bar", "baz", new Exception());
+        String[] lines = buf.toString().split(NEWLINE);
+        assertEquals("test: Foo. [bar=baz]", lines[0]);
+        assertEquals("java.lang.Exception", lines[1]);
+    }
+
+    protected static StringWriter configWriterImpl () {
+        StringWriter buf = new StringWriter();
+        final PrintWriter out = new PrintWriter(buf);
+        Logger.setImpl(new Logger.Impl() {
+            public void debug (String message, Throwable t) {
+                out.println(message);
+                if (t != null) t.printStackTrace(out);
+            }
+            public void info (String message, Throwable t) {
+                out.println(message);
+                if (t != null) t.printStackTrace(out);
+            }
+            public void warning (String message, Throwable t) {
+                out.println(message);
+                if (t != null) t.printStackTrace(out);
+            }
+        });
+        return buf;
+    }
+
+        protected static final String NEWLINE = System.getProperty("line.separator");
+}
