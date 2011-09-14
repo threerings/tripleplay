fa533265ea51cc1962704fcd965008cf5c845fb6
Charlie Groves <charlie.groves@gmail.com>
2011-09-14 15:39:21 -0700

    Reheadered

--- src/main/java/tripleplay/ui/Log.java
+++ src/main/java/tripleplay/ui/Log.java
@@ -1,6 +1,7 @@
 //
-// Atlantis - tile laying fun for the whole family!
-// https://github.com/threerings/atlantis
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
 
 package tripleplay.ui;
 
