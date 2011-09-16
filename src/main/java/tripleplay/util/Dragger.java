bef7ca491302c4aa3d2c4fe320888ea3beb34d35
Michael Bayne <mdb@samskivert.com>
2011-09-16 10:30:16 -0700

    Further simplification of the Dragger, added some docs.
    Better I think to just make it a fancy pointer listener and have the user
    simply override hitTest and hasExpired on their desired Input.Region if they
    desire control over drag starting and dragger expiry.

--- src/main/java/tripleplay/util/Dragger.java
+++ src/main/java/tripleplay/util/Dragger.java
@@ -12,70 +12,57 @@ import pythagoras.f.IPoint;
 import pythagoras.f.Point;
 
 /**
- * A helpful class for implementing drag and drop behavior.
+ * A helpful class for implementing drag and drop behavior. Use in conjunction with an input region
+ * like so:
+ * <pre>{@code
+ * input.register(new SizedLayerRegion(layer) {
+ *   @Override public boolean hitTest (IPoint point) {
+ *     return //can start drag// && super.hitTest(point);
+ *   }
+ *   // also override hasExpired if you desire custom expiry
+ * }, new Dragger() {
+ *   @Override public viod onDragStart (IPoint start) { // drag bits // }
+ *   // etc.
+ * });
+ * }</pre>
  */
 public abstract class Dragger
-    implements Input.Region, Pointer.Listener
+    implements Pointer.Listener
 {
     /**
-     * Called when a pointer click triggers our start region to check whether we are allowed to
-     * start a drag right now or not.
-     * @return true if a drag is allowed, false otherwise.
-     */
-    public boolean canDrag () {
-        return true;
-    }
-
-    /**
      * Called when a drag has started.
-     * @param startLoc the point at which the drag started.
+     * @param start the point at which the drag started.
      */
-    public void onDragStart (IPoint startLoc) {
+    public void onDragStart (IPoint start) {
     }
 
     /**
      * Called when a drag is in progress.
-     * @param curLoc the current pointer position.
-     * @param startLoc the point at which the drag started.
+     * @param current the current pointer position.
+     * @param start the point at which the drag started.
      */
-    public void onDragged (IPoint curLoc, IPoint startLoc) {
+    public void onDragged (IPoint current, IPoint start) {
     }
 
     /**
      * Called when the pointer was released during a drag.
-     * @param curLoc the current (and final) pointer position.
-     * @param startLoc the point at which the drag started.
+     * @param current the current (and final) pointer position.
+     * @param start the point at which the drag started.
      */
-    public void onDragEnd (IPoint curLoc, IPoint startLoc) {
-    }
-
-    public Dragger (Input.Region region) {
-        _region = region;
-    }
-
-    @Override public boolean hasExpired () {
-        return _region.hasExpired();
-    }
-
-    @Override public boolean hitTest (IPoint p) {
-        return canDrag() && _region.hitTest(p);
+    public void onDragEnd (IPoint current, IPoint start) {
     }
 
     @Override public void onPointerStart (Pointer.Event event) {
-        _startLoc = new Point(event.x(), event.y());
-        onDragStart(_startLoc);
+        onDragStart(_start.set(event.x(), event.y()));
     }
 
     @Override public void onPointerDrag (Pointer.Event event) {
-        onDragged(_curLoc.set(event.x(), event.y()), _startLoc);
+        onDragged(_current.set(event.x(), event.y()), _start);
     }
 
     @Override public void onPointerEnd (Pointer.Event event) {
-        onDragEnd(_curLoc.set(event.x(), event.y()), _startLoc);
+        onDragEnd(_current.set(event.x(), event.y()), _start);
     }
 
-    protected Input.Region _region;
-
-    protected Point _startLoc;
-    protected Point _curLoc = new Point();
+    protected Point _start = new Point(), _current = new Point();
 }
