f83f4477a25588b3fcd7fa49dd771c9b4ff1b19f
Michael Bayne <mdb@samskivert.com>
2011-09-16 09:55:24 -0700

    Revamp of Input	based on Tim's suggestions.
    There's no longer a distinction between a reactor and a bounded pointer
    listener. A reactor now defines a hit test and expiry functions, and provides a
    pointer listener to be notified when a pointer click triggers the reactor.
    Subsequent drag and release events are dispatched to the triggered reactor's
    listener.
    
    Action is now an adapted Pointer.Listener that simply calls "onTrigger" when
    the pointer is clicked and discards drag and release events.

--- src/main/java/tripleplay/util/Input.java
+++ src/main/java/tripleplay/util/Input.java
@@ -15,38 +15,78 @@ import pythagoras.f.IPoint;
 import pythagoras.f.IRectangle;
 import pythagoras.f.Point;
 
-import tripleplay.util.Coords;
-
 /**
  * Dispatches user input to the appropriate entity.
  */
 public class Input
 {
-    /** Provides a handle on an action registration. */
+    /** Provides a handle on a listener or action registration. */
     public interface Registration {
         /** Unregisters the action associated with this handle. */
         void cancel ();
     }
 
-    /** Encapsulates an action to be taken on user input. */
-    public interface Action {
+    /** A listener that will call {@link Action#onTrigger} when the pointer is pressed. */
+    public static abstract class Action extends Pointer.Adapter {
         /** Called when the user triggers a reactor. */
-        void onTrigger ();
+        public abstract void onTrigger ();
+
+        @Override public void onPointerStart (Pointer.Event event) {
+            onTrigger();
+        }
     }
 
-    /** Provides fine-grained control over the reaction process for clients that need it. */
-    public interface Reactor {
-        /** Returns true if this reactor is no longer relevant and should be removed. */
-        boolean hasExpired ();
-        /** Returns true if the (screen-coordinates) point triggers this reactor. */
-        boolean hitTest (IPoint p);
-        /** Executes the reactor's action. */
-        void onTrigger ();
+    /** Encapsulates hit testing and reactor expiry. */
+    public static abstract class Reactor {
+        /** The pointer listener that receives events for this reactor. */
+        public final Pointer.Listener listener;
+
+        /** Returns true if this listener is no longer relevant and should be removed. */
+        public abstract boolean hasExpired ();
+
+        /** Returns true if the (screen-coordinates) point triggers this listener. */
+        public abstract boolean hitTest (IPoint p);
+
+        public Reactor (Pointer.Listener listener) {
+            this.listener = listener;
+        }
     }
 
-    /** Implements reaction for a layer and a bounding box. */
-    public static abstract class LayerReactor implements Reactor {
-        public LayerReactor (Layer layer, IRectangle bounds) {
+    /** A reactor that responds to clicks anywhere on the entire screen. */
+    public static class ScreenReactor extends Reactor {
+        public ScreenReactor (Pointer.Listener listener) {
+            super(listener);
+        }
+
+        @Override public boolean hasExpired () {
+            return false;
+        }
+        @Override public boolean hitTest (IPoint p) {
+            return true;
+        }
+    }
+
+    /** A reactor that responds to clicks in the supplied (screen) bounds. */
+    public static class BoundsReactor extends ScreenReactor {
+        public BoundsReactor (IRectangle bounds, Pointer.Listener listener) {
+            super(listener);
+            _bounds = bounds;
+        }
+
+        @Override public boolean hitTest (IPoint p) {
+            return _bounds.contains(p);
+        }
+
+        protected IRectangle _bounds;
+    }
+
+    /** A reactor that responds to clicks in the supplied bounds, as transformed by the supplied
+     * layer's transform. While the layer in question is not visible, the reactor will not be
+     * notified. If this reactor is considered for processing and its layer has been removed from
+     * the view hierarchy, it will automatically be canceled. */
+    public static class LayerReactor extends Reactor {
+        public LayerReactor (Layer layer, IRectangle bounds, Pointer.Listener listener) {
+            super(listener);
             _layer = layer;
             _bounds = bounds;
         }
@@ -54,126 +94,128 @@ public class Input
         @Override public boolean hasExpired () {
             return _layer.parent() == null;
         }
-
         @Override public boolean hitTest (IPoint p) {
             if (!_layer.visible()) return false;
             // convert the screen coordinates into layer-relative coordinates and check that the
             // point falls within the (layer-transform-relative) bounds
-            return _bounds.contains(Coords.screenToLayer(_layer, p, new Point()));
+            return _bounds.contains(Layer.Util.screenToLayer(_layer, p, new Point()));
         }
 
         protected Layer _layer;
         protected IRectangle _bounds;
     }
 
+    /** A reactor that responds to clicks in the supplied layer's (transformed) bounds. While the
+     * layer in question is not visible, the reactor will not be notified. If this reactor is
+     * considered for processing and its layer has been removed from the view hierarchy, it will
+     * automatically be canceled. */
+    public static class SizedLayerReactor extends Reactor {
+        public SizedLayerReactor (Layer.HasSize layer, Pointer.Listener listener) {
+            super(listener);
+            _layer = layer;
+        }
+
+        @Override public boolean hasExpired () {
+            return _layer.parent() == null;
+        }
+        @Override public boolean hitTest (IPoint p) {
+            if (!_layer.visible()) return false;
+            // convert the screen coordinates into layer-relative coordinates
+            Point lp = Layer.Util.screenToLayer(_layer, p, new Point());
+            float x = lp.x, y = lp.y;
+            return (x > 0 && y > 0 && x < _layer.scaledWidth() && y < _layer.scaledHeight());
+        }
+
+        protected Layer.HasSize _layer;
+    }
+
     /** Receives input from the PlayN Pointer service. */
     public final Pointer.Listener plistener = new Pointer.Listener() {
         @Override public void onPointerStart (Pointer.Event event) {
-            float x = event.x(), y = event.y();
-            // see if any of our reactors consume this click
-            Point p = new Point(x, y);
-            // take a snapshot of the reactors list at the start of the click to avoid
-            // concurrent modification if reactors are added or removed during processing
-            List<Reactor> reactorsSnapshot = new ArrayList<Reactor>(_reactors);
-            for (int ii = reactorsSnapshot.size()-1; ii >= 0; ii--) {
-                Reactor r = reactorsSnapshot.get(ii);
+            // take a snapshot of the reactors list to avoid concurrent modification if reactors
+            // are added or removed during processing
+            List<Reactor> snapshot = new ArrayList<Reactor>(_reactors);
+            Point p = new Point(event.x(), event.y());
+            for (int ii = snapshot.size() - 1; ii >= 0; ii--) {
+                Reactor r = snapshot.get(ii);
                 if (r.hasExpired()) {
                     _reactors.remove(r);
                 } else if (r.hitTest(p)) {
-                    r.onTrigger();
-                    return;
-                }
-            }
-
-            // if no reactors consume the click, potentially start a BPL
-            for (int ii = _listeners.size()-1; ii >= 0; ii--) {
-                BPL bpl = _listeners.get(ii);
-                if (bpl.bounds.contains(x, y)) {
-                    _activeBPL = bpl;
-                    _activeBPL.listener.onPointerStart(event);
+                    _active = r;
+                    r.listener.onPointerStart(event);
                     break;
                 }
             }
         }
 
         @Override public void onPointerDrag (Pointer.Event event) {
-            if (_activeBPL != null) {
-                _activeBPL.listener.onPointerDrag(event);
+            if (_active != null) {
+                _active.listener.onPointerDrag(event);
             }
         }
 
         @Override public void onPointerEnd (Pointer.Event event) {
-            if (_activeBPL != null) {
-                _activeBPL.listener.onPointerEnd(event);
-                _activeBPL = null;
+            if (_active != null) {
+                _active.listener.onPointerEnd(event);
+                _active = null;
             }
         }
 
-        protected BPL _activeBPL;
+        protected Reactor _active;
     };
 
     /**
-     * Configures a listener to be notified of pointer activity that does not trigger a reactor. On
-     * pointer start, such listeners will be scanned from most-recently-registered to
-     * least-recently-registered and checked for bounds intersections. Subsequent pointer drag and
-     * end events will be dispatched to the listener that matched the pointer start.
-     */
-    public Registration register (IRectangle bounds, Pointer.Listener listener) {
-        final BPL bpl = new BPL(bounds, listener);
-        _listeners.add(bpl);
-        return new Registration() {
-            public void cancel () {
-                _listeners.remove(bpl);
-            }
-        };
-    }
-
-    /**
-     * Registers an input action to be taken when the user clicks or presses in the supplied
-     * bounding rectangle, as transformed by the supplied layer's transform. While the layer in
-     * question is not visible, the action will not be triggered.
+     * Configures a reactor to be notified of pointer activity. On pointer start, reactors will be
+     * scanned from most-recently-registered to least-recently-registered and hit-tested. Thus more
+     * recently registered reactors that overlap previously registered reactors will take
+     * precedence. TODO: use layer depth information to hit test based on depth.
      *
-     * <p> Note: if an action is considered for processing and its layer has been removed from the
-     * view hierarchy, the action will automatically be canceled. </p>
-     *
-     * @return a handle that can be used to clear this registration.
-     */
-    public Registration register (Layer layer, IRectangle bounds, final Action action) {
-        return register(new LayerReactor(layer, bounds) {
-            public void onTrigger () {
-                action.onTrigger();
-            }
-        });
-    }
-
-    /**
-     * Registers a reactor. More recently registered reactors will be checked before older
-     * reactors, and will thus be preferred in the case of overlap. Presently there's no other way
-     * to control overlap behavior.
+     * <p> Subsequent pointer drag and end events will be dispatched to the reactor that
+     * successfully hit-tested the pointer start. </p>
      *
      * @return a handle that can be used to clear this registration.
      */
     public Registration register (final Reactor reactor) {
         _reactors.add(reactor);
         return new Registration() {
-            public void cancel () {
+            @Override public void cancel () {
                 _reactors.remove(reactor);
             }
         };
     }
 
-    protected static class BPL {
-        public final IRectangle bounds;
-        public Pointer.Listener listener;
-        public BPL (IRectangle bounds, Pointer.Listener listener) {
-            this.bounds = bounds;
-            this.listener = listener;
-        }
+    /**
+     * Registers a @{link ScreenReactor}.
+     * @return a handle that can be used to clear this registration.
+     */
+    public Registration register (Pointer.Listener listener) {
+        return register(new ScreenReactor(listener));
+    }
+
+    /**
+     * Registers a @{link BoundsReactor}.
+     * @return a handle that can be used to clear this registration.
+     */
+    public Registration register (IRectangle bounds, Pointer.Listener listener) {
+        return register(new BoundsReactor(bounds, listener));
+    }
+
+    /**
+     * Registers a {@link LayerReactor}.
+     * @return a handle that can be used to clear this registration.
+     */
+    public Registration register (Layer layer, IRectangle bounds, Pointer.Listener listener) {
+        return register(new LayerReactor(layer, bounds, listener));
+    }
+
+    /**
+     * Registers a {@link SizedLayerReactor}.
+     * @return a handle that can be used to clear this registration.
+     */
+    public Registration register (Layer.HasSize layer, Pointer.Listener listener) {
+        return register(new SizedLayerReactor(layer, listener));
     }
 
     /** A list of all registered reactors. */
     protected List<Reactor> _reactors = new ArrayList<Reactor>();
-
-    /** A list of all registered bounded pointer listeners. */
-    protected List<BPL> _listeners = new ArrayList<BPL>();
 }
