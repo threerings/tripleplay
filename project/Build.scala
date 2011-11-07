5736d02d2a781e2761224b09a0e55117d94c0515
Michael Bayne <mdb@samskivert.com>
2011-11-07 09:17:32 -0800

    Prepare for 1.0 release.

--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,9 @@ import Keys._
 
 object TriplePlayBuild extends Build {
   val locals = com.samskivert.condep.Depends(
-    ("react",      null,  "com.threerings" % "react" % "1.1-SNAPSHOT"),
-    ("playn",     "core", "com.googlecode.playn" % "playn-core" % "1.0-SNAPSHOT"),
-    ("playn",     "java", "com.googlecode.playn" % "playn-java" % "1.0-SNAPSHOT" % "test")
+    ("react",      null,  "com.threerings" % "react" % "1.1"),
+    ("playn",     "core", "com.googlecode.playn" % "playn-core" % "1.0"),
+    ("playn",     "java", "com.googlecode.playn" % "playn-java" % "1.0" % "test")
   )
 
   lazy val tripleplay = locals.addDeps(Project(
