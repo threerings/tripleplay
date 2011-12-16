066934b82bf5bc78c1c2fbc4ba37cab5a1f59a9f
Michael Bayne <mdb@samskivert.com>
2011-12-16 15:24:09 -0800

    Added Shim, for great space taking up.

---
+++ src/main/java/tripleplay/ui/Shim.java
@@ -0,0 +1,33 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.ui;
+
+import pythagoras.f.Dimension;
+import pythagoras.f.Point;
+
+/**
+ * An invisible widget that simply requests a fixed amount of space.
+ */
+public class Shim extends Element<Shim>
+{
+    public Shim (float width, float height) {
+        _shimWidth = width;
+        _shimHeight = height;
+    }
+
+    @Override protected Dimension computeSize (float hintX, float hintY) {
+        return new Dimension(_shimWidth, _shimHeight);
+    }
+
+    @Override protected void layout () {
+    }
+
+    @Override protected Element<?> hitTest (Point point) {
+        return null; // don't intercept clicks by default
+    }
+
+    protected final float _shimWidth, _shimHeight;
+}
