ebc75965d552fde021f53e06af0ba65d33f42ee4
Michael Bayne <mdb@samskivert.com>
2012-03-01 15:48:36 -0800

    A mechanism for firing one-shot and periodic timers.

---
+++ src/main/java/tripleplay/util/Timer.java
@@ -0,0 +1,124 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+import playn.core.PlayN;
+
+/**
+ * Handles execution of actions after a specified delay.
+ */
+public class Timer
+{
+    /** A handle on registered actions that can be used to cancel them. */
+    public static interface Handle {
+        /** Cancels the action in question. */
+        void cancel ();
+    }
+
+    /** Creates a timer instance that can be used to schedule actions.
+     * Don't forget to call {@link #update} on it, every frame. */
+    public Timer () {
+        this(System.currentTimeMillis());
+    }
+
+    /** Executes the supplied action after the specified number of milliseconds have elapsed.
+     * @return a handle that can be used to cancel the execution of the action. */
+    public Handle after (int millis, Runnable action) {
+        return add(millis, 0, action);
+    }
+
+    /** Executes the supplied action starting {@code millis} from now and every {@code millis}
+     * thereafter.
+     * @return a handle that can be used to cancel the execution of the action. */
+    public Handle every (int millis, Runnable action) {
+        return atThenEvery(millis, millis, action);
+    }
+
+    /** Executes the supplied action starting {@code initialMillis} from now and every {@code
+     * repeatMillis} there after.
+     * @return a handle that can be used to cancel the execution of the action. */
+    public Handle atThenEvery (int initialMillis, int repeatMillis, Runnable action) {
+        return add(initialMillis, repeatMillis, action);
+    }
+
+    /** This should be called from {@link Game#update}, or similar. */
+    public void update () {
+        update(System.currentTimeMillis());
+    }
+
+    // this and update(long) exist so that we can unit test this class
+    protected Timer (long now) {
+        _currentTime = now;
+    }
+
+    protected void update (long now) {
+        _currentTime = now;
+
+        Action root = _root;
+        while (root.next != null && root.next.nextExpire <= now) {
+            Action act = root.next;
+            try {
+                act.action.run();
+            } catch (Exception e) {
+                PlayN.log().warn("Action failed", e);
+            }
+            if (act.repeatMillis == 0) {
+                root.next = act.next;
+                act.next = null;
+            } else {
+                act.nextExpire += act.repeatMillis;
+                root.next = insert(act, act.next);
+            }
+        }
+    }
+
+    protected Handle add (int initialMillis, int repeatMillis, Runnable action) {
+        Action act = new Action(initialMillis, repeatMillis, action);
+        _root.next = insert(act, _root.next);
+        return act;
+    }
+
+    protected Action insert (Action target, Action next) {
+        if (next == null || next.nextExpire > target.nextExpire) {
+            target.next = next;
+            return target;
+        } else {
+            next.next = insert(target, next.next);
+            return next;
+        }
+    }
+
+    protected Action remove (Action target, Action next) {
+        if (target == next) return target.next;
+        else if (next == null) return null;
+        else return remove(target, next.next);
+    }
+
+    protected class Action implements Handle {
+        public final int repeatMillis;
+        public final Runnable action;
+
+        public long nextExpire;
+        public Action next;
+
+        public Action (int initialMillis, int repeatMillis, Runnable action) {
+            this.nextExpire = _currentTime + initialMillis;
+            this.repeatMillis = repeatMillis;
+            this.action = action;
+        }
+
+        @Override public void cancel () {
+            _root.next = remove(this, _root.next);
+        }
+
+        @Override public String toString () {
+            return nextExpire + "/" + repeatMillis + "/" + action + " -> " + next;
+        }
+    }
+
+    protected final Action _root = new Action(0, 0, null);
+    protected long _currentTime;
+}
---
+++ src/test/java/tripleplay/util/TimerTest.java
@@ -0,0 +1,109 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TimerTest
+{
+    @Test
+    public void testOneShot () {
+        Timer timer = new Timer(0);
+        int[] r1 = new int[1];
+        Timer.Handle h1 = timer.after(10, action(r1));
+        // make sure the timer hasn't run yet
+        timer.update(3);
+        assertEquals(0, r1[0]);
+        // elapse time past our expirey and make sure it has run
+        timer.update(15);
+        assertEquals(1, r1[0]);
+    }
+
+    @Test
+    public void testCancelBefore () {
+        Timer timer = new Timer(0);
+        int[] r1 = new int[1];
+        Timer.Handle h1 = timer.after(10, action(r1));
+        // make sure the timer hasn't run yet
+        timer.update(3);
+        assertEquals(0, r1[0]);
+        h1.cancel();
+        // elapse time past our expirey and make sure our canceled timer did not run
+        timer.update(15);
+        assertEquals(0, r1[0]);
+    }
+
+    @Test
+    public void testRepeat () {
+        long time = 25;
+        Timer timer = new Timer(time);
+        int[] r1 = new int[1];
+        Timer.Handle h1 = timer.every(10, action(r1));
+        // make sure the timer hasn't run yet
+        timer.update(time += 3);
+        assertEquals(0, r1[0]);
+        // elapse time past our expirey and make sure our action ran
+        timer.update(time += 10);
+        assertEquals(1, r1[0]);
+        // elapse time past our expirey again and make sure our action ran again
+        timer.update(time += 10);
+        assertEquals(2, r1[0]);
+        // cancel our timer and make sure it ceases to run
+        h1.cancel();
+        timer.update(time += 10);
+        assertEquals(2, r1[0]);
+    }
+
+    @Test
+    public void testMultiple () {
+        long time = 0;
+        Timer timer = new Timer(time);
+        int[] r1 = new int[1], r2 = new int[2], r3 = new int[3];
+        // set up timers to expire after 10, 10, and 25, the latter two repeating every 10 and 15
+        Timer.Handle h1 = timer.after(10, action(r1));
+        Timer.Handle h2 = timer.every(10, action(r2));
+        Timer.Handle h3 = timer.atThenEvery(25, 15, action(r3));
+
+        // T=T+3: no timers have run
+        time += 3; timer.update(time);
+        assertEquals(0, r1[0]);
+        assertEquals(0, r2[0]);
+        assertEquals(0, r3[0]);
+
+        // T=T+13; h1 and h2 expire, h3 doesn't
+        time += 10; timer.update(time);
+        assertEquals(1, r1[0]);
+        assertEquals(1, r2[0]);
+        assertEquals(0, r3[0]);
+
+        // T=T+23; h1 is gone, h2 expires again, h3 not yet
+        time += 10; timer.update(time);
+        assertEquals(1, r1[0]);
+        assertEquals(2, r2[0]);
+        assertEquals(0, r3[0]);
+
+        // T=T+33; h1 is gone, h2 expires again, h3 expires once
+        time += 10; timer.update(time);
+        assertEquals(1, r1[0]);
+        assertEquals(3, r2[0]);
+        assertEquals(1, r3[0]);
+
+        // T=T+43; h2 expires again, h3 expires again
+        time += 10; timer.update(time);
+        assertEquals(1, r1[0]);
+        assertEquals(4, r2[0]);
+        assertEquals(2, r3[0]);
+    }
+
+    protected Runnable action (final int[] ranCount) {
+        return new Runnable() {
+            public void run () {
+                ranCount[0] += 1;
+            }
+        };
+    }
+}
