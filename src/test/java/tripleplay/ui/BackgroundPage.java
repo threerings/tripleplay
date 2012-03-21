48da7822df24614c398f0be93f1a731fa1fb278d
Michael Bayne <mdb@samskivert.com>
2012-03-21 12:17:52 -0700

    Use a table layout, and a helper method to reduce the noise.

--- src/test/java/tripleplay/ui/BackgroundPage.java
+++ src/test/java/tripleplay/ui/BackgroundPage.java
@@ -7,9 +7,8 @@ package tripleplay.ui;
 
 import playn.core.Image;
 import playn.core.PlayN;
-import tripleplay.ui.bgs.NullBackground;
-import tripleplay.ui.bgs.Scale9Background;
 import tripleplay.ui.layout.AxisLayout;
+import tripleplay.ui.layout.TableLayout;
 
 public class BackgroundPage implements WidgetDemo.Page
 {
@@ -20,46 +19,28 @@ public class BackgroundPage implements WidgetDemo.Page
     public Group createInterface () {
         Image testBg = PlayN.assets().getImage("images/background.png");
         Image scale9Bg = PlayN.assets().getImage("images/scale9.png");
-        return new Group(AxisLayout.vertical()).add(
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77, 10)))).add(
-                        new Label("Beveled")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77, 0)))).add(
-                        new Label("Beveled (no inset)"))),
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.solid(0xFFCCFF99, 10)))).add(new Label("Solid")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.solid(0xFFCCFF99)))).add(new Label("Solid (no inset)"))),
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    new NullBackground(10)))).add(new Label("Null")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    new NullBackground()))).add(new Label("Null (no inset)"))),
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.image(testBg, 10)))).add(new Label("Image")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.image(testBg, 0)))).add(new Label("Image (no inset)"))),
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.scale9(scale9Bg, 5)))).add(new Label("Scale 9")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.scale9(scale9Bg, 5)))).add(
-                        new Label("Scale 9\nSomewhat\nTaller\nAnd\nWider",
-                            Styles.make(Style.HALIGN.center)))),
-            new Group(AxisLayout.horizontal()).add(
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2, 10)))).add(
-                        new Label("Bordered (inset 10)")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2, 2)))).add(
-                        new Label("Bordered (inset 2)")),
-                new Group(AxisLayout.horizontal(), Styles.make(Style.BACKGROUND.is(
-                    Background.bordered(0xFFEEEEEE, 0xFFFFFF, 2)))).add(
-                        new Label("Bordered (no inset)")))
-        );
+        return new Group(new TableLayout(3).gaps(5, 5)).add(
+            label("Beveled", Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77, 10)),
+            label("Beveled (no inset)", Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77, 0)),
+            new Label(),
+            label("Solid", Background.solid(0xFFCCFF99, 10)),
+            label("Solid (no inset)", Background.solid(0xFFCCFF99)),
+            new Label(),
+            label("Null", Background.blank(10)),
+            label("Null (no inset)", Background.blank(0)),
+            new Label(),
+            label("Image", Background.image(testBg, 10)),
+            label("Image (no inset)", Background.image(testBg, 0)),
+            new Label(),
+            label("Scale 9", Background.scale9(scale9Bg, 5)),
+            label("Scale 9\nSomewhat\nTaller\nAnd\nWider", Background.scale9(scale9Bg, 5)),
+            new Label(),
+            label("Bordered (inset 10)", Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2, 10)),
+            label("Bordered (inset 2)", Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2, 2)),
+            label("Bordered (no inset)", Background.bordered(0xFFEEEEEE, 0xFFFFFF, 2)));
+    }
+
+    protected Label label (String text, Background bg) {
+        return new Label(text).addStyles(Style.HALIGN.center, Style.BACKGROUND.is(bg));
     }
 }
