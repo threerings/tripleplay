69f06c6d25602b5d7e998ceb03b7556496e57cfe
Michael Bayne <mdb@samskivert.com>
2012-07-18 09:23:53 -0700

    Added codec for enums.

--- core/src/main/java/tripleplay/syncdb/Codec.java
+++ core/src/main/java/tripleplay/syncdb/Codec.java
@@ -10,6 +10,25 @@ package tripleplay.syncdb;
  */
 public interface Codec<T>
 {
+    /** A codec for enums which encodes to/from {@link Enum#name}. */
+    class EnumC<E extends Enum<E>> implements Codec<E> {
+        public static <E extends Enum<E>> EnumC<E> create (Class<E> eclass) {
+            return new EnumC<E>(eclass);
+        }
+
+        public EnumC (Class<E> eclass) {
+            _eclass = eclass;
+        }
+        @Override public String encode (E value) {
+            return value.name();
+        }
+        @Override public E decode (String data) {
+            return Enum.valueOf(_eclass, data);
+        }
+        protected final Class<E> _eclass;
+    }
+
+    /** A codec for strings. The identity codec. */
     Codec<String> STRING = new Codec<String>() {
         public String encode (String value) {
             return value;
@@ -19,6 +38,7 @@ public interface Codec<T>
         }
     };
 
+    /** A codec for ints. */
     Codec<Integer> INT = new Codec<Integer>() {
         public String encode (Integer value) {
             return String.valueOf(value);
@@ -28,6 +48,7 @@ public interface Codec<T>
         }
     };
 
+    /** A codec for longs. */
     Codec<Long> LONG = new Codec<Long>() {
         public String encode (Long value) {
             return String.valueOf(value);
@@ -37,6 +58,7 @@ public interface Codec<T>
         }
     };
 
+    /** A codec for booleans. Encodes to the string {@code true} or {@code false}. */
     Codec<Boolean> BOOLEAN = new Codec<Boolean>() {
         public String encode (Boolean value) {
             return String.valueOf(value);
