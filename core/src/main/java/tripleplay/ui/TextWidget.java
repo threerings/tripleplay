213a5149bd905e8b48d04f632f286d773310b515
Michael Bayne <mdb@samskivert.com>
2012-08-06 09:04:08 -0700

    This invalidate() call is not necessary.
    When an interface hierarchy is added, it is invalidated and laid out
    automatically. When an element is added to an already added group, the group is
    automatically invalidated and relaid out.

--- core/src/main/java/tripleplay/ui/TextWidget.java
+++ core/src/main/java/tripleplay/ui/TextWidget.java
@@ -67,11 +67,6 @@ public abstract class TextWidget<T extends TextWidget<T>> extends Widget<T>
         };
     }
 
-    @Override protected void wasAdded (Elements<?> parent) {
-        super.wasAdded(parent);
-        invalidate();
-    }
-
     @Override protected void wasRemoved () {
         super.wasRemoved();
         _tglyph.destroy();
