fb54fae21c24b1f11e06fe99773906b855040ddc
Michael Bayne <mdb@samskivert.com>
2012-08-22 16:47:39 -0700

    Added logger for anim package, not currently used.

---
+++ core/src/main/java/tripleplay/anim/Log.java
@@ -0,0 +1,17 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.anim;
+
+import tripleplay.util.Logger;
+
+/**
+ * Provides logging for this package.
+ */
+class Log
+{
+    /** Provides logging for this package. */
+    public static final Logger log = new Logger("tpanim");
+}
