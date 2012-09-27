1c7a49212ba2b6b15c3beb08ff67eafab6fd0844
Nathan Curtis <nathan.curtis@gmail.com>
2012-09-27 12:22:42 -0700

    We do need a threshold for moves.
    It's incredibly hard to tap and release without moving your finger at all.

--- core/src/main/java/tripleplay/gesture/Tap.java
+++ core/src/main/java/tripleplay/gesture/Tap.java
@@ -5,8 +5,10 @@
 
 package tripleplay.gesture;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
+import pythagoras.f.Point;
 
 /**
  * A simple touch gesture. May support 1 to 4 fingers. If greedy, will indicate a held touch, and
@@ -27,7 +29,7 @@ public class Tap extends GestureBase<Tap>
     }
 
     @Override protected void clearMemory () {
-        _touchIds.clear();
+        _startPoints.clear();
     }
 
     @Override protected void updateState (GestureNode node) {
@@ -38,10 +40,9 @@ public class Tap extends GestureBase<Tap>
             break;
 
         case MOVE:
-            // TODO: this may need some mitigating circumstances such as moves that are very small
-            // or waiting to see if they release quickly enough after the initial start.
-            // For now: any move immediately disqualifies us.
-            setState(State.UNQUALIFIED);
+            Point start = _startPoints.get(node.touch.id());
+            if (start == null) Log.log.warning("No start for a moved touch", "id", node.touch.id());
+            else if (start.distance(node.location()) > MOVE_THRESHOLD) setState(State.UNQUALIFIED);
             break;
 
         case CANCEL:
@@ -49,8 +50,8 @@ public class Tap extends GestureBase<Tap>
             break;
 
         case START:
-            _touchIds.add(node.touch.id());
-            int size = _touchIds.size();
+            _startPoints.put(node.touch.id(), node.location());
+            int size = _startPoints.size();
             if (size > _touches) setState(State.UNQUALIFIED);
             else if (size == _touches && _greedy) setState(State.GREEDY);
             break;
@@ -58,12 +59,15 @@ public class Tap extends GestureBase<Tap>
         case END:
             // when any touch ends, we either move to unqualified if we didn't have enough touches
             // or complete if we did.
-            setState(_touchIds.size() == _touches ? State.COMPLETE : State.UNQUALIFIED);
+            setState(_startPoints.size() == _touches ? State.COMPLETE : State.UNQUALIFIED);
             break;
         }
     }
 
+    // They must move this far to be disqualified for a tap
+    protected static final int MOVE_THRESHOLD = 10;
+
     protected final int _touches;
 
-    protected Set<Integer> _touchIds = new HashSet<Integer>();
+    protected Map<Integer, Point> _startPoints = new HashMap<Integer, Point>();
 }
