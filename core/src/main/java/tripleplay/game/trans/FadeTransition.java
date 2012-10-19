befefa32ca758c7cd231af9b95020e91606be25c
mthomas <mthomas@threerings.net>
2012-10-19 11:42:13 -0700

    Add a really simple transition that fades in a new screen.
    The old screen is left at full opacity while the new screen fades in front from 0 - 1 alpha.

---
+++ core/src/main/java/tripleplay/game/trans/FadeTransition.java
@@ -0,0 +1,29 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011-2012, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.game.trans;
+
+import tripleplay.game.Screen;
+import tripleplay.game.ScreenStack;
+
+/**
+ * Fades the new screen in front of the old one.
+ */
+public class FadeTransition extends InterpedTransition<SlideTransition>
+{
+    public FadeTransition (ScreenStack stack) {
+    }
+
+    @Override public boolean update (Screen oscreen, Screen nscreen, float elapsed) {
+        float nalpha = _interp.apply(0, 1, elapsed, _duration);
+        nscreen.layer.setAlpha(nalpha);
+        return elapsed >= _duration;
+    }
+
+    @Override public void complete (Screen oscreen, Screen nscreen) {
+        super.complete(oscreen, nscreen);
+        nscreen.layer.setAlpha(1);
+    }
+}
