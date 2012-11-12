b1bcfe4cbcb8f855d3f75ec25d582c77cfac02c7
Jamie Doornbos <jamie@threerings.net>
2012-11-11 17:06:00 -0800

    Do simple task execution in Interface
    This is technically do-able with Animator, but would be excessively quirky. This will be used
    in application of scrolling friction and fading scroll bars.

--- core/src/main/java/tripleplay/ui/Interface.java
+++ core/src/main/java/tripleplay/ui/Interface.java
@@ -10,18 +10,32 @@ import java.util.List;
 
 import playn.core.Game;
 import playn.core.GroupLayer;
+import react.Slot;
 import tripleplay.anim.Animator;
 import tripleplay.ui.Element.Flag;
 
 /**
  * The main class that integrates the Triple Play UI with a PlayN game. This class is mainly
- * necessary to handle the proper dispatching of input events to UI elements. Create an interface
- * instance, create {@link Root} groups via the interface and add the {@link Root#layer}s into your
- * scene graph wherever you desire. Call {@link #update} and {@link #paint} from {@link
- * Game#update} and {@link Game#paint} to render your interface.
+ * necessary to automatically validate hierarchies of {@code Element}s during each paint.
+ * Create an interface instance, create {@link Root} groups via the interface and add the
+ * {@link Root#layer}s into your scene graph wherever you desire. Call {@link #update} and 
+ * {@link #paint} from {@link Game#update} and {@link Game#paint} to render your interface.
  */
 public class Interface
 {
+    /**
+     * A time based task that requires an update per frame.
+     * @see Interface#addTask(Task)
+     */
+    public interface Task {
+        /**
+         * Performs the update for this task.
+         * @param delta time that has passed, normally passed down from
+         * {@link playn.core.Game#update(float)}
+         */
+        void update (float delta);
+    }
+
     /**
      * Posts a runnable that will be executed after the next time the interface is validated.
      * Processing deferred actions is not tremendously efficient, so don't call this every frame.
@@ -31,9 +45,64 @@ public class Interface
     }
 
     /**
+     * Adds a task that will henceforth be updated once per game update. If a task is added during
+     * the task update iteration, it will be updated for the first time on the following game
+     * update.
+     */
+    public void addTask (Task task) {
+        _tasks.add(task);
+    }
+
+    /**
+     * Removes a previously added task. If a task is removed during an update and has not yet
+     * been updated itself, then it will not be updated.
+     */
+    public void removeTask (Task task) {
+        int idx = _tasks.indexOf(task);
+        if (idx == -1) return;
+        _tasks.remove(idx);
+        // adjust iteration members
+        if (_currentTask >= idx) _currentTask--;
+        _currentTaskCount--;
+    }
+
+    /**
+     * Ensures that the given task is always added to the interface's tasks when the given element
+     * is added to a root.
+     * @return the element for chaining
+     * @see Element#hierarchyChanged()
+     */
+    public <T extends Element<?>> T updateWhenAdded (final T elem, final Task task) {
+        Slot<Boolean> hierChange = new Slot<Boolean>() {
+            @Override public void onEmit (Boolean added) {
+                if (added) addTask(task);
+                else removeTask(task);
+            }
+        };
+        elem.hierarchyChanged().connect(hierChange);
+        if (elem.isAdded()) hierChange.onEmit(true);
+        return elem;
+    }
+
+    /**
+     * Single parameter version of {@link #updateWhenAdded(Element, Task)} for use when the
+     * {@code Element} also happens to be a task.
+     * @param elem the Element and Task
+     * @return the element for chaining
+     */
+    public <T extends Element<?> & Task> T updateWhenAdded (final T elem) {
+        return updateWhenAdded(elem, elem);
+    }
+
+    /**
      * Updates the elements in this interface. Must be called from {@link Game#update}.
      */
     public void update (float delta) {
+        // use members for task iteration to support concurrent modification
+        for (_currentTask = 0, _currentTaskCount = _tasks.size(); _currentTask < _currentTaskCount;
+                _currentTask++) _tasks.get(_currentTask).update(delta);
+        _currentTask = -1;
+        // update animator
         _elapsed += delta;
         _animator.update(_elapsed);
     }
@@ -130,5 +199,7 @@ public class Interface
     protected final List<Root> _dispatch = new ArrayList<Root>();
     protected final List<Runnable> _actions = new ArrayList<Runnable>();
     protected final Animator _animator = Animator.create();
+    protected final List<Task> _tasks = new ArrayList<Task>();
     protected float _elapsed;
+    protected int _currentTask, _currentTaskCount;
 }
