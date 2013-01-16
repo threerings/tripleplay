ddd3707b5992f83304957ad35097819066816886
David Hoover <karma@deadmoose.com>
2013-01-16 13:52:18 -0800

    Auld Lang Syne

--- core/src/main/java/tripleplay/ui/MenuHost.java
+++ core/src/main/java/tripleplay/ui/MenuHost.java
@@ -1,3 +1,8 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011-2013, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
 package tripleplay.ui;
 
 import playn.core.Connection;
--- demo/core/src/main/java/tripleplay/demo/util/ColorsDemo.java
+++ demo/core/src/main/java/tripleplay/demo/util/ColorsDemo.java
@@ -1,6 +1,6 @@
 //
 // Triple Play - utilities for use in PlayN-based games
-// Copyright (c) 2011-2012, Three Rings Design, Inc. - All rights reserved.
+// Copyright (c) 2011-2013, Three Rings Design, Inc. - All rights reserved.
 // http://github.com/threerings/tripleplay/blob/master/LICENSE
 
 package tripleplay.demo.util;
