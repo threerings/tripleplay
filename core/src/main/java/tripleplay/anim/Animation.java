45e08790294440c2add0304d422f0206b2807264
Michael Bayne <mdb@samskivert.com>
2013-02-25 16:24:17 -0800

    Point reader to AnimGroup re: parallel animations.

--- core/src/main/java/tripleplay/anim/Animation.java
+++ core/src/main/java/tripleplay/anim/Animation.java
@@ -357,15 +357,7 @@ public abstract class Animation
      * Returns a builder for constructing an animation that will be queued up for execution when
      * the current animation completes. <em>Note:</em> only a single animation can be chained on
      * this returned animation builder. You cannot queue up multiple animations to fire in parallel
-     * using the animation builder returned by this method. If you need to do that, do this:
-     * <pre>{@code
-     * rootAnim.queueSomeAnim().then().action(new Runnable() {
-     *   public void run () {
-     *     rootAnim.queueAnimA()...
-     *   }
-     * }).then().queueAnimB()...
-     * }</pre>
-     * This will result in {@code queueAnimA} and {@code queueAnimB} being started in parallel.
+     * using the builder returned by this method. Use {@link AnimGroup} for that.
      */
     public AnimBuilder then () {
         return new ChainBuilder() {
