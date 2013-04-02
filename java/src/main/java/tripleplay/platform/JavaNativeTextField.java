7d99a86e7681f7fc2a40166ac5cadb614d378fb4
mthomas <mthomas@threerings.net>
2013-04-02 14:54:52 -0700

    Make the native java text field respect secureness and font styles.
    Due to java having a different class entirely for JPasswordField, we have to sometimes recreate the field entirely to switch to the other implementation.

--- java/src/main/java/tripleplay/platform/JavaNativeTextField.java
+++ java/src/main/java/tripleplay/platform/JavaNativeTextField.java
@@ -10,16 +10,18 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+import pythagoras.f.FloatMath;
 import pythagoras.f.IRectangle;
 
-import playn.core.PlayN;
 import playn.core.Font;
 import playn.core.Keyboard;
 
+import react.Connection;
 import react.Signal;
 import react.Slot;
 import react.Value;
@@ -29,6 +31,23 @@ public class JavaNativeTextField
 {
     public JavaNativeTextField (Container root) {
         _root = root;
+
+        setupField();
+    }
+
+    protected void setupField () {
+        if (_textConnection != null) _textConnection.disconnect();
+
+        JTextField oldField = _field;
+
+        // Use an appropriate field type based on our security mode.
+        _field = _isSecure ? new JPasswordField() : new JTextField();
+
+        // Restore any of our settings that we can directly from the old field.
+        if (oldField != null) {
+            _field.setBounds(oldField.getBounds());
+            _field.setFont(oldField.getFont());
+        }
 
         _field.getDocument().addDocumentListener(new DocumentListener() {
             public void changedUpdate (DocumentEvent event) {
@@ -52,11 +71,10 @@ public class JavaNativeTextField
         _field.setBorder(BorderFactory.createEmptyBorder());
         _field.setBackground(new Color(0xffffff, false)); // TODO(bruno): Transparency
 
-        _text.connect(new Slot<String>() { public void onEmit (String value) {
-            if (!_field.getText().equals(value)) {
-                _field.setText(value);
-            }
-        }});
+        _textConnection = _text.connectNotify(new Slot<String>() {
+            @Override public void onEmit (String value) {
+                if (!_field.getText().equals(value)) _field.setText(value);
+            }});
     }
 
     @Override public Value<String> text () {
@@ -83,7 +101,8 @@ public class JavaNativeTextField
     }
 
     @Override public JavaNativeTextField setFont (Font font) {
-        // TODO
+        _field.setFont(new java.awt.Font(font.name(), awtFontStyle(font.style()),
+            FloatMath.round(font.size())));
         return this;
     }
 
@@ -104,7 +123,11 @@ public class JavaNativeTextField
     }
 
     @Override public JavaNativeTextField setSecureTextEntry (boolean enable) {
-        // TODO
+        if (_isSecure != enable) {
+            _isSecure = enable;
+            setupField();
+        }
+
         return this;
     }
 
@@ -120,9 +143,27 @@ public class JavaNativeTextField
         _field.requestFocus();
     }
 
+    protected static int awtFontStyle (Font.Style style)
+    {
+        switch (style) {
+        case BOLD: return java.awt.Font.BOLD;
+        case BOLD_ITALIC: return java.awt.Font.BOLD | java.awt.Font.ITALIC;
+        case ITALIC: return java.awt.Font.ITALIC;
+        case PLAIN: return java.awt.Font.PLAIN;
+        }
+
+        // Unknown style, use plain.
+        return java.awt.Font.PLAIN;
+    }
+
+
     protected Container _root;
-    protected JTextField _field = new JTextField();
+    protected JTextField _field;
 
     protected Value<String> _text = Value.create("");
     protected Signal<Boolean> _finishedEditing = Signal.create();
+
+    protected boolean _isSecure;
+
+    protected Connection _textConnection;
 }
