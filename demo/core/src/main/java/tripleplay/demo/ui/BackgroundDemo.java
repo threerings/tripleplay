1187a4a2b29b0a6e96535928a5fe60cfd15886a0
David Hoover <karma@deadmoose.com>
2013-10-23 15:34:58 -0700

    Jump through async hoops for the demo's scale 9 bg.
    Poking my rebuild of the demos for the web made me realize
    that this didn't work on the html version there

--- demo/core/src/main/java/tripleplay/demo/ui/BackgroundDemo.java
+++ demo/core/src/main/java/tripleplay/demo/ui/BackgroundDemo.java
@@ -7,6 +7,7 @@ package tripleplay.demo.ui;
 
 import playn.core.Image;
 import playn.core.PlayN;
+import playn.core.util.Callback;
 
 import tripleplay.ui.Background;
 import tripleplay.ui.Group;
@@ -14,7 +15,6 @@ import tripleplay.ui.Label;
 import tripleplay.ui.Style;
 import tripleplay.ui.layout.TableLayout;
 import tripleplay.util.Colors;
-
 import tripleplay.demo.DemoScreen;
 
 public class BackgroundDemo extends DemoScreen
@@ -28,7 +28,22 @@ public class BackgroundDemo extends DemoScreen
 
     @Override protected Group createIface () {
         Image testBg = PlayN.assets().getImage("images/background.png");
-        Image scale9Bg = PlayN.assets().getImageSync("images/scale9.png");
+
+        // Have to do async shenanigans with scale9 backgrounds
+        final Label scale9Labels[] = {
+            label("Scale 9", Background.blank()),
+            label("Scale 9\nSomewhat\nTaller\nAnd\nWider", Background.blank()),
+        };
+        PlayN.assets().getImageSync("images/scale9.png").addCallback(new Callback<Image>() {
+            public void onSuccess (Image image) {
+                for (Label label : scale9Labels) {
+                    label.addStyles(Style.BACKGROUND.is(Background.scale9(image).inset(5)));
+                }
+            }
+
+            public void onFailure (Throwable cause) { }
+        });
+
         return new Group(new TableLayout(3).gaps(5, 5)).add(
             label("Beveled", Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77).inset(10)),
             label("Beveled (no inset)", Background.beveled(0xFFCCFF99, 0xFFEEFFBB, 0xFFAADD77)),
@@ -45,8 +60,8 @@ public class BackgroundDemo extends DemoScreen
             label("Image", Background.image(testBg).inset(10)),
             label("Image (no inset)", Background.image(testBg)),
             new Label(),
-            label("Scale 9", Background.scale9(scale9Bg).inset(5)),
-            label("Scale 9\nSomewhat\nTaller\nAnd\nWider", Background.scale9(scale9Bg).inset(5)),
+            scale9Labels[0],
+            scale9Labels[1],
             new Label(),
             label("Bordered (inset 10)", Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2).inset(10)),
             label("Bordered (inset 2)", Background.bordered(0xFFEEEEEE, 0xFFFFFF00, 2).inset(2)),
