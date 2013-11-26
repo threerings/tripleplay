065d64542708e2b22e62d4e96b716579b86e6ed1
Michael Bayne <mdb@samskivert.com>
2013-11-26 11:18:37 -0800

    An unordered bag, optimized for the speeds.

---
+++ core/src/main/java/tripleplay/util/Bag.java
@@ -0,0 +1,144 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011-2013, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+import java.util.Iterator;
+
+/**
+ * An unordered collection of elements which may contain duplicates. Elements must not be null. The
+ * elements will be reordered during normal operation of the bag. This is optimized for fast
+ * additions, removals and iteration. It is not optimized for programmer ass coverage; see the
+ * warnings below.
+ *
+ * <p><em>Note:</em> extra bounds checking is <em>not performed</em> which means that some invalid
+ * operations will succeeed and return null rather than throwing {@code IndexOutOfBoundsException}.
+ * Be careful.</p>
+ *
+ * <p><em>Note:</em> the iterator returned by {@link #iterator} does not make concurrent
+ * modification checks, so concurrent modifications will cause unspecified behavior. Don't do
+ * that.</p>
+ */
+public class Bag<E> implements Iterable<E>
+{
+    /** Creates an empty bag. This allows one to avoid repeating the parameter type:
+     * {@code Bag<Foo> bag = Bag.create();}
+     */
+    public static <E> Bag<E> create () {
+        return new Bag<E>();
+    }
+
+    /** Creates an empty bag with the specified initial capacity. This allows one to avoid
+     * repeating the parameter type: {@code Bag<Foo> bag = Bag.create();}
+     */
+    public static <E> Bag<E> create (int initialCapacity) {
+        return new Bag<E>(initialCapacity);
+    }
+
+    /** Creates a bag with a default initial capacity of 16. */
+    public Bag () {
+        this(16);
+    }
+
+    /** Creates a bag with the specified initial capacity. */
+    public Bag (int initialCapacity) {
+        _elems = new Object[initialCapacity];
+    }
+
+    /** Returns the number of elements in this bag.*/
+    public int size () {
+        return _size;
+    }
+
+    /** Returns whether this bag is empty. */
+    public boolean isEmpty () {
+        return _size == 0;
+    }
+
+    /** Returns the element at {@code index}. */
+    public E get (int index) {
+        @SuppressWarnings("unchecked") E elem = (E)_elems[index];
+        return elem;
+    }
+
+    /** Returns whether this bag contains {@code elem}. Equality is by reference. */
+    public boolean contains (E elem) {
+        for (int ii = 0, ll = _size; ii < ll; ii++) {
+            if (elem == _elems[ii]) return true;
+        }
+        return false;
+    }
+
+    /** Adds {@code elem} to this bag. The element will always be added to the end of the bag.
+     * @return the index at which the element was added.
+     */
+    public int add (E elem) {
+        if (_size == _elems.length) expand(_elems.length*3/2+1);
+        _elems[_size++] = elem;
+        return _size;
+    }
+
+    /** Removes the element at the specified index.
+     * @return the removed element. */
+    public E removeAt (int index) {
+        @SuppressWarnings("unchecked") E elem = (E)_elems[index];
+        _elems[index] = _elems[--_size];
+        _elems[_size] = null;
+        return elem;
+    }
+
+    /** Removes the first occurrance of {@code elem} from the bag. Equality is by reference.
+     * @return true if {@code elem} was found and removed, false if not.
+     */
+    public boolean remove (E elem) {
+        for (int ii = 0, ll = _size; ii < ll; ii++) {
+            Object ee = _elems[ii];
+            if (ee == elem) {
+                _elems[ii] = _elems[--_size];
+                _elems[_size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Removes and returns the last element of the bag.
+     * @throws ArrayIndexOutOfBoundsException if the bag is empty. */
+    public E removeLast () {
+        @SuppressWarnings("unchecked") E elem = (E)_elems[_size-1];
+        _elems[--_size] = null;
+        return elem;
+    }
+
+    /** Removes all elements from this bag. */
+    public void removeAll () {
+        for (int ii = 0; ii < _size; ii++) _elems[ii] = null;
+        _size = 0;
+    }
+
+    @Override public Iterator<E> iterator () {
+        return new Iterator<E>() {
+            @Override public boolean hasNext() {
+                return _pos < _size;
+            }
+            @Override public E next () {
+                return get(_pos++);
+            }
+            @Override public void remove () {
+                Bag.this.removeAt(--_pos);
+            }
+            protected int _pos;
+        };
+    }
+
+    private void expand (int capacity) {
+        Object[] elems = new Object[capacity];
+        System.arraycopy(_elems, 0, elems, 0, _elems.length);
+        _elems = elems;
+    }
+
+    protected Object[] _elems;
+    protected int _size;
+}
---
+++ core/src/test/java/tripleplay/util/BagTest.java
@@ -0,0 +1,43 @@
+//
+// Triple Play - utilities for use in PlayN-based games
+// Copyright (c) 2011-2013, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/tripleplay/blob/master/LICENSE
+
+package tripleplay.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BagTest
+{
+    @Test public void testAddContainsRemove () {
+        Bag<Integer> bag = Bag.create();
+        for (int ii = 15; ii < 35; ii++) {
+            bag.add(ii);
+        }
+        assertEquals(35-15, bag.size());
+        for (int ii = 0; ii < 50; ii++) {
+            assertEquals(ii >= 15 && ii < 35, bag.contains(ii));
+        }
+        for (int ii = 0; ii < 50; ii++) {
+            assertEquals(ii >= 15 && ii < 35, bag.remove(ii));
+        }
+        assertEquals(0, bag.size());
+    }
+
+    @Test public void testIterator () {
+        Bag<Integer> bag = Bag.create();
+        Set<Integer> values = new HashSet<Integer>();
+        values.add(5);
+        values.add(10);
+        values.add(25);
+
+        for (Integer elem : values) bag.add(elem);
+        assertEquals(values.size(), bag.size());
+        for (Integer elem : bag) assertTrue(values.remove(elem));
+        assertTrue(values.isEmpty());
+    }
+}
