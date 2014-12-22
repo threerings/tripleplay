4f16e6d0add63b2836ca14a993c719a1e4fea88a
Michael Bayne <mdb@samskivert.com>
2014-12-22 11:00:25 -0800

    Interpolator simplification.
    They all effectively operate on a single value (v) between 0 and 1. That value
    can be obtained via dt/t when you're driving your interpolation via elapsed
    time, and the result can be converted to start + result * range when you want
    your result to be in [start, start+range]. But none of those extra bits need to
    be in the interp code, obscuring the basic math.

--- core/src/main/java/tripleplay/util/Interpolator.java
+++ core/src/main/java/tripleplay/util/Interpolator.java
@@ -14,103 +14,122 @@ public abstract class Interpolator
 {
     /** An interpolator that always returns the starting position. */
     public static Interpolator NOOP = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            return start;
+        @Override public String toString () { return "NOOP"; }
+        @Override public float apply (float v) {
+            return 0;
         }
     };
 
     /** A linear interpolator. */
     public static Interpolator LINEAR = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
-            return start + range * dt / t;
+        @Override public String toString () { return "LINEAR"; }
+        @Override public float apply (float v) {
+            return v;
         }
     };
 
     /** An interpolator that starts to change slowly and ramps up to full speed. */
     public static Interpolator EASE_IN = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
-            float dtt = dt / t;
-            return start + range * dtt * dtt * dtt;
+        @Override public String toString () { return "EASE_IN"; }
+        @Override public float apply (float v) {
+            return v * v * v;
         }
     };
 
     /** An interpolator that starts to change quickly and eases into the final value. */
     public static Interpolator EASE_OUT = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
-            float dtt = dt / t - 1;
-            return start + range * (1 + dtt * dtt * dtt);
+        @Override public String toString () { return "EASE_OUT"; }
+        @Override public float apply (float v) {
+            float vv = v - 1;
+            return (1 + vv * vv * vv);
         }
     };
 
     /** An interpolator that eases away from the starting value, speeds up, then eases into the
      * final value. */
     public static Interpolator EASE_INOUT = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
-            float hdtt = dt / (t/2);
-            if (hdtt < 1) {
-                return start + range/2 * hdtt * hdtt * hdtt;
-            } else {
-                float nhdtt = hdtt - 2;
-                return start + range/2 * (2 + nhdtt * nhdtt * nhdtt);
+        @Override public String toString () { return "EASE_INOUT"; }
+        @Override public float apply (float v) {
+            float v2 = 2*v;
+            if (v2 < 1) {
+                return (v2 * v2 * v2)/2;
             }
+            float ov = v2 - 2;
+            return (2 + ov * ov * ov)/2;
         }
     };
 
     /** An interpolator that undershoots the starting value, then speeds up into the final value */
     public static Interpolator EASE_IN_BACK = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
+        @Override public String toString () { return "EASE_IN_BACK"; }
+        @Override public float apply (float v) {
             float curvature = 1.70158f;
-            float dtt = dt / t;
-            return range * dtt*dtt * ((curvature+1) * dtt - curvature) + start;
+            return v * v * ((curvature+1) * v - curvature);
         }
     };
 
     /** An interpolator that eases into the final value and overshoots it before settling on it. */
     public static Interpolator EASE_OUT_BACK = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (t == 0) return start + range;
-            float curvature = 1.70158f;
-            float dtt = dt / t - 1;
-            return range * (dtt*dtt * ((curvature + 1) * dtt + curvature) + 1) + start;
+        @Override public String toString () { return "EASE_OUT_BACK"; }
+        @Override public float apply (float v) {
+            float curvature = 1.70158f, v1 = v - 1;
+            return (v1 * v1 * ((curvature+1) * v1 + curvature) + 1);
         }
     };
 
     public static Interpolator BOUNCE_OUT = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            float dtt = dt / t;
-            if ((dtt) < (1/2.75f)) {
-                return range*(7.5625f*dtt*dtt) + start;
-            } else if (dtt < (2/2.75f)) {
-                float dttBounce = dtt - (1.5f/2.75f);
-                return range*(7.5625f*dttBounce*dttBounce + .75f) + start;
-            } else if (dtt < (2.5/2.75)) {
-                float dttBounce = dtt - (2.25f/2.75f);
-                return range*(7.5625f*dttBounce*dttBounce + .9375f) + start;
+        @Override public String toString () { return "BOUNCE_OUT"; }
+        @Override public float apply (float v) {
+            if (v < (1/2.75f)) {
+                return 7.5625f * v * v;
+            } else if (v < (2/2.75f)) {
+                float vBounce = v - (1.5f/2.75f);
+                return 7.5625f * vBounce * vBounce + 0.75f;
+            } else if (v < (2.5/2.75)) {
+                float vBounce = v - (2.25f/2.75f);
+                return 7.5625f * vBounce * vBounce + 0.9375f;
             } else {
-                float dttBounce = dtt - (2.625f/2.75f);
-                return range*(7.5625f*dttBounce*dttBounce + .984375f) + start;
+                float vBounce = v - (2.625f/2.75f);
+                return 7.5625f * vBounce * vBounce + 0.984375f;
             }
         }
     };
 
     /** An interpolator that eases past the final value then back towards it elastically. */
     public static Interpolator EASE_OUT_ELASTIC = new Interpolator() {
-        @Override public float apply (float start, float range, float dt, float t) {
-            if (dt==0) return range;
-            float dtt = dt / t;
-            if (dtt == 1) return range+start;
-            float p = t * .3f;
-            float a = start;
-            float s = p/4;
-            return (a*(float)Math.pow(2,-10*dtt) *
-                (float)Math.sin((dtt*t-s) * (2*(float)Math.PI)/p) + start + range);
+        @Override public String toString () { return "EASE_OUT_ELASTIC"; }
+        @Override public float apply (float v) {
+            return (float)Math.pow(2, -10 * v) * (float)Math.sin((v - K) * J) + 1;
         }
+        private static final float K = 0.3f/4;
+        private static final float J = (float)(2*Math.PI/0.3);
     };
+
+    /**
+     * Interpolates between zero and one according to this interpolator's function.
+     *
+     * @param v a value between zero and one (usually {@code elapsed/total} time).
+     */
+    public abstract float apply (float v);
+
+    /**
+     * Interpolates between zero and one according to this interpolator's function.
+     *
+     * @param dt the amount of time that has elapsed.
+     * @param t the total amount of time for the interpolation. If t == 0, the result is undefined.
+     */
+    public float apply (float dt, float t) {
+        return apply(dt/t);
+    }
+
+    /**
+     * Interpolates between two values, as in {@link #apply(float,float)} except that {@code dt} is
+     * clamped to [0..t] to avoid interpolation weirdness if {@code dt} is ever negative or exceeds
+     * {@code t}.
+     */
+    public float applyClamp (float dt, float t) {
+        return apply((dt < 0) ? 0 : (dt > 1 ? 1 : dt), t);
+    }
 
     /**
      * Interpolates between two values.
@@ -121,7 +140,10 @@ public abstract class Interpolator
      * @param t the total amount of time for the interpolation. If t == 0, start+range will be
      * returned.
      */
-    public abstract float apply (float start, float range, float dt, float t);
+    public float apply (float start, float range, float dt, float t) {
+        float pos = (t == 0) ? 1 : apply(dt, t);
+        return start + range * pos;
+    }
 
     /**
      * Interpolates between two values, as in {@link #apply} except that {@code dt} is clamped to
