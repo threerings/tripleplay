163990c3e619522ad78d52863ed5c5e48cb0a41f
Michael Bayne <mdb@samskivert.com>
2015-06-03 10:41:05 -0700

    Only bind to our value when we're added to the UI.
    This allows us to display values which outlive our UI without worrying that
    garbage views will keep listening and responding to value changes.

--- core/src/main/java/tripleplay/ui/ValueLabel.java
+++ core/src/main/java/tripleplay/ui/ValueLabel.java
@@ -5,6 +5,7 @@
 
 package tripleplay.ui;
 
+import react.Closeable;
 import react.ValueView;
 
 /**
@@ -19,15 +20,25 @@ public class ValueLabel extends TextWidget<ValueLabel>
     public final ValueView<?> text;
 
     /** Creates a label with the supplied value. The value will be converted to a string for
-     * display as this label's text. */
+      * display as this label's text. */
     public ValueLabel (ValueView<?> text) {
-        text.connect(textDidChange());
-        // TODO: connect in onAdd, disconnect in onRemove?
         this.text = text;
+    }
+
+    @Override protected void wasAdded () {
+        super.wasAdded();
+        _conn = text.connect(textDidChange());
+    }
+
+    @Override protected void wasRemoved () {
+        super.wasRemoved();
+        _conn.close();
     }
 
     @Override public String toString () { return "VLabel(" + text.get() + ")"; }
     @Override protected Class<?> getStyleClass () { return Label.class; }
     @Override protected String text () { return String.valueOf(text.get()); }
     @Override protected Icon icon () { return null; }
+
+    protected Closeable _conn;
 }
