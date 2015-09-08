40eba0d61ef889d8411a398dcc5e54925c62ed46
Michael Bayne <mdb@samskivert.com>
2015-09-08 08:24:57 -0700

    Disallow overwriting constraints.
    This is never the right thing and is almost certainly going to lead to
    unexpected behavior and confusion.
    
    Since we're still in the transition from 1.x to 2.x, and because this is very
    unlikely to affect "working" code, I don't feel quite so bad about introducing
    runtime freakoutery.

--- core/src/main/java/tripleplay/ui/Element.java
+++ core/src/main/java/tripleplay/ui/Element.java
@@ -281,6 +281,9 @@ public abstract class Element<T extends Element<T>>
      * @return this element for call chaining.
      */
     public T setConstraint (Layout.Constraint constraint) {
+        if (_constraint != null && constraint != null) throw new IllegalStateException(
+            "Cannot set constraint on element which already has a constraint. " +
+                "Layout constraints cannot be automatically composed.");
         if (constraint != null) constraint.setElement(this);
         _constraint = constraint;
         invalidate();
