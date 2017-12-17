333db922ffe412178d19ce9e2ae9e2acc86b9973
Michael Bayne <mdb@samskivert.com>
2017-12-17 11:05:03 -0800

    Remove ye olde ios backend, add robovm backend.

---
+++ demo/robovm/src/main/java/tripleplay/demo/TripleDemoRoboVM.java
@@ -0,0 +1,41 @@
+package tripleplay.demo;
+
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIInterfaceOrientationMask;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIWindow;
+
+import playn.robovm.RoboPlatform;
+
+public class TripleDemoRoboVM extends UIApplicationDelegateAdapter {
+
+  @Override
+  public boolean didFinishLaunching (UIApplication app, UIApplicationLaunchOptions launchOpts) {
+    // create a full-screen window
+    CGRect bounds = UIScreen.getMainScreen().getBounds();
+    UIWindow window = new UIWindow(bounds);
+
+    // configure and create the PlayN platform
+    RoboPlatform.Config config = new RoboPlatform.Config();
+    config.orients = UIInterfaceOrientationMask.All;
+    RoboPlatform plat = RoboPlatform.create(window, config);
+
+    // create and initialize our game
+    new TripleDemo(plat);
+
+    // make our main window visible (this starts the platform)
+    window.makeKeyAndVisible();
+    addStrongRef(window);
+    return true;
+  }
+
+  public static void main (String[] args) {
+    NSAutoreleasePool pool = new NSAutoreleasePool();
+    UIApplication.main(args, null, TripleDemoRoboVM.class);
+    pool.close();
+  }
+}
